3bda32d9c6f7e2cd35842ea51d900fc7369f9f0e
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-10 12:17:23 +0600

    date function

---
+++ date.php
@@ -0,0 +1,19 @@
+<?php
+	echo date("m"); //Month
+	echo date("Y"); // Year
+	echo date("l"); // Day of the week
+
+	echo "<br>";
+
+	echo date("Y/m/d");
+	echo "<br>";
+	echo date("m-d-Y");
+	echo "<br>";
+	echo date("h"); // hour
+	echo date("i"); // min
+	echo date("s"); //seconds
+
+	echo date("a"); //AM or PM
+
+	echo date("h:i:sa");
+?>
