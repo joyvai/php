ef0f585bc64fc854ce25571aeeb06f33270e40f2
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-10 12:20:42 +0600

    include and require function.

---
+++ website1/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My website</title>
+</head>
+<body>
+		<ul>
+			<li><a href="index.php">Home1</li>
+			<li><a href="contact.php">Contact</li>
+			<li><a href="about.php">About</li>
+		</ul>
+	
