e5264309683796b3336622a5fc022757b1f80dd7
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-10 12:15:21 +0600

    edit arrays

---
+++ arrays.php
@@ -0,0 +1,102 @@
+<?php
+
+// Indexed
+
+$people = array('kevin','Jeremy','Sara');
+$ids = array(23,55,12);
+$cars = ['Honda','Toyota','Ford'];
+$cars[3] = 'Chevy';
+$cars[] = 'BMW';
+
+
+// if i want to count how many cars
+// use count function
+
+echo count($cars)."<br>";
+print_r($cars);
+var_dump($cars);
+
+
+
+// associative arrays 
+
+//key-value pairs
+
+$arrayName = array('joy' => 23, 'mama' => 24, 'rabbit' => 22 );
+
+print_r($arrayName);
+
+$arrayName['faldua'] = 222;
+var_dump($arrayName); 
+
+// multi-Dimensional
+
+// array in array
+
+$Cars = array(
+	array('Honda', 20, 10),
+	array('BMW',200,199),
+	array('Ford',2000,1990)
+);
+
+echo $Cars[0][2];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
