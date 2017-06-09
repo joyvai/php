dcbae473433c00417a3de444de8b14384b9cc66c
Iftekharjoy <iftekharjoy1@gmail.com>
2017-06-10 01:59:50 +0600

    added

---
+++ predefined_var.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- UTF = UNICODE TRANSFORMATION FORMAT -->
+	<!-- 8 BIT UTF ENCODING IS BEING USED -->
+	<meta charset="utf-8">
+	<title>Predefined Variables</title>
+</head>
+<body>
+<pre>
+	<?php 
+		//$_SERVER stores all sorts of data about server
+		print_r($_SERVER);
+		phpinfo();
+
+	?>
+</pre>
+
+</body>
+</html>
---
+++ variable/var.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variables</title>
+</head>
+<body>
+<?php
+$street = "100 main street";
+$city = "State College";
+$state = "PA";
+$zip = 16801;
+
+print "<p>The address is:<br>$street<br>city:$city<br>State: $state<br>Zip: $zip</p>"
+?>
+</body>
+</html>
