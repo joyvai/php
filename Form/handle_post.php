fff902e9c291f4df1262f9560f65d8f45f5e8c21
Iftekharjoy <iftekharjoy1@gmail.com>
2017-06-14 22:51:51 +0600

    Modified Handle_post

--- Form/handle_post.php
+++ Form/handle_post.php
@@ -15,9 +15,14 @@
 
 	//create a full name variable:
 	$name = $first_name . ' ' . $last_name;
+	// // Adjust for HTML tags:
+	// $html_post = htmlentities($_POST['posting']);
+	// $strip_post = strip_tags($_POST['posting']);
 	//print a message:
-	print "<div>Thank you, $name, for your posting:<p>$posting</p></div>";
-
+	print "<div>Thank you, $name, for your posting.<p>$posting</p></div>";
+	$name=urlencode($name);
+	$email=urlencode($_POST['email']);
+	print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a>to continue.</p>"
 
 ?>
 </body>
